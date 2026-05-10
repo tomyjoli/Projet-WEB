753b6efe49d54ff627bfbe8152b97120f1329676
tomyjoli <tom.garrido@efrei.net>
2026-05-10 12:06:06 +0200

    fichier formation en bonne voie

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,21 +1,24 @@
-// On attend que le DOM soit chargé
 document.addEventListener('DOMContentLoaded', () => {
     const slides = document.querySelectorAll('.slide');
-    let currentIndex = 0;
-    const intervalTime = 5000; // 5000ms = 5 secondes
+    if (slides.length > 0) {
+        let currentIndex = 0;
+        const intervalTime = 5000;
 
-    function nextSlide() {
-        // 1. Retirer la classe active de l'image actuelle
-        slides[currentIndex].classList.remove('active');
-
-        // 2. Calculer l'index de la photo suivante
-        // Le modulo (%) permet de revenir à 0 quand on arrive à la fin
-        currentIndex = (currentIndex + 1) % slides.length;
-
-        // 3. Ajouter la classe active à la nouvelle image
-        slides[currentIndex].classList.add('active');
+        function nextSlide() {
+            slides[currentIndex].classList.remove('active');
+            currentIndex = (currentIndex + 1) % slides.length;
+            slides[currentIndex].classList.add('active');
+        }
+        setInterval(nextSlide, intervalTime);
     }
 
-    // Lancer l'intervalle automatique
-    setInterval(nextSlide, intervalTime);
+    const items = document.querySelectorAll('.toggle-item');
+
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+
+            item.classList.toggle('active');
+            
+        });
+    });
 });
